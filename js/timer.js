332f23fc24dc626410dab678476ff716c7ba701d
iCharlieAraiza <eliptikman1234@gmail.com>
2021-11-19 05:55:44 -0600

    Test accurate timer

--- js/timer.js
+++ js/timer.js
@@ -1,17 +1,72 @@
-let time = 20
-let breakTime = 10
 
-const timer = document.querySelector('.timer .time')
+// Define Pomodoro time
+// Define Break time
 
-let seconds = time;
 
-const interval = setInterval(()=>{
-    if(seconds <= 0){
-        if(breakTime <= 0){
-            clearInterval(interval)
-        }   
-        timer.innerHTML = breakTime--
+// Define current Real time 
+
+//Get the real time (mm);
+// 
+
+
+const startBtn = document.querySelector('#start')
+
+let on = true;
+let interval;
+let currentTimer = 60;
+let actualTime  = 0;
+
+const timerStartEvent = ()=>{
+    let time;
+
+    if(on){
+        let currentTimerStart = Date.now();
+        interval = setInterval(()=>{
+            let time = (Date.now() - currentTimerStart);
+            time = Math.floor(time / 1000)
+            actualTime = currentTimer - time;
+            console.log(currentTimer - time)
+            document.querySelector('title').textContent = actualTime
+        },1000)
     }else{
-        timer.innerHTML = seconds--
+        currentTimer = actualTime;
+        clearInterval(interval)
     }
-},1000)
+    on = !on;
+}
+
+
+startBtn.addEventListener('click', timerStartEvent)
+
+
+/*
+
+let interval = ""
+
+
+interval = setInterval(function() {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+
+function getRemainingTime(endTime) {
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+  
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total / 60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+  
+    return {
+      total,
+      minutes,
+      seconds,
+    };
+  }
+
+  */
