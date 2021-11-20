795259cf3b697b14ca57c37fb608c8368e391cf1
iCharlieAraiza <eliptikman1234@gmail.com>
2021-11-19 23:50:44 -0600

    Alfa V3. Fix timer and add menu button

--- js/timer.js
+++ js/timer.js
@@ -10,23 +10,38 @@
 
 
 const startBtn = document.querySelector('#start')
+const stopBtn = document.querySelector('#stop')
+
+
+let pomodoroTimeValue = 10
+let breaTimeValue = 30
+
 
 let on = true;
 let interval;
-let currentTimer = 60;
+let currentTimer = pomodoroTimeValue;
 let actualTime  = 0;
+let breakTime = breaTimeValue;
+breakSet = true;
 
 const timerStartEvent = ()=>{
     let time;
-
     if(on){
         let currentTimerStart = Date.now();
         interval = setInterval(()=>{
             let time = (Date.now() - currentTimerStart);
             time = Math.floor(time / 1000)
             actualTime = currentTimer - time;
-            console.log(currentTimer - time)
-            document.querySelector('title').textContent = actualTime
+            console.log(calculateTime(actualTime))
+            document.querySelector('title').textContent = calculateTime(actualTime)
+            if(actualTime <= 0 && breakSet){
+                currentTimer = breakTime
+                currentTimerStart = Date.now()+10;
+                breakSet = false
+            }else if(actualTime === 0){
+                breakSet = true;
+                clearInterval(interval)
+            }
         },1000)
     }else{
         currentTimer = actualTime;
@@ -36,37 +51,29 @@ const timerStartEvent = ()=>{
 }
 
 
-startBtn.addEventListener('click', timerStartEvent)
-
-
-/*
-
-let interval = ""
-
-
-interval = setInterval(function() {
-    timer.remainingTime = getRemainingTime(endTime);
-    updateClock();
-
-    total = timer.remainingTime.total;
-    if (total <= 0) {
-      clearInterval(interval);
+const calculateTime = value => {
+    const sec = parseInt(value, 10); 
+    let hours = Math.floor(sec / 3600); 
+    let minutes = Math.floor((sec - hours * 3600) / 60); 
+    let seconds = sec - hours * 3600 - minutes * 60;
+    if (hours < 10) {      hours = '0' + hours;    }
+    if ( (hours * 60) + minutes < 10 ) { minutes = `0${minutes}`}
+    if (seconds < 10) {      seconds = '0' + seconds;    }
+    if (hours == 0) {
+    return minutes + ':' + seconds; // Return in MM:SS format
+    } else {
+    return  ( (hours * 60) + minutes) + ':' + seconds; // Return in HH:MM:SS format
     }
-  }, 1000);
+}
 
-function getRemainingTime(endTime) {
-    const currentTime = Date.parse(new Date());
-    const difference = endTime - currentTime;
-  
-    const total = Number.parseInt(difference / 1000, 10);
-    const minutes = Number.parseInt((total / 60) % 60, 10);
-    const seconds = Number.parseInt(total % 60, 10);
-  
-    return {
-      total,
-      minutes,
-      seconds,
-    };
-  }
 
-  */
+const resetTimer = () =>{
+    currentTimer = pomodoroTimeValue
+    actualTime = 0
+    on = true
+    clearInterval(interval)
+}
+
+
+startBtn.addEventListener('click', timerStartEvent)
+stopBtn.addEventListener('click', resetTimer)
